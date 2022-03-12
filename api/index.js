691eaa2fbfe9c80e4543d0d0642d08f8b10a4fe4
Cyryl Majewski <c.majewski@twoheads.pl>
2022-03-12 20:51:10 +0100

    fix(test): test

--- api/index.js
+++ api/index.js
@@ -1,19 +1,36 @@
-import * as curlconverter from 'curlconverter';
-import express from 'express';
-import bodyParser from "body-parser";
+// import * as curlconverter from 'curlconverter';
+// import express from 'express';
+// import bodyParser from "body-parser";
 
-const port = 3000;
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
+// const port = 3000;
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/python', (req, res) => {
-    const { curlUrl } = req.body
+// app.post('/python', (req, res) => {
+//     const { curlUrl } = req.body
 
-    const result = curlconverter.toPython(curlUrl);
+//     const result = curlconverter.toPython(curlUrl);
 
-    res.end(JSON.stringify(result));
-})
+//     res.end(JSON.stringify(result));
+// })
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+// app.listen(port, () => {
+//     console.log(`App listening on port ${port}`);
+// });
+
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
